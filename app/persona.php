15bedd837a444e18770a01aa9f188b46039f72be
Henrryrj <henrryrocajoffre@gmail.com>
2021-01-20 12:54:32 -0400

    Arregalndo los errores de migration xd

--- app/persona.php
+++ app/persona.php
@@ -4,7 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class persona extends Model
+class Persona extends Model
 {
-    //
+    protected $table ='persona';
+    protected $primaryKey='id';
+    public $timestamps=false;
+
+    protected $fillable=[
+        'id',
+        'ci',
+        'nombre',
+        'apellido',
+        'direccion',
+        'fechaDeNacimiento',
+        'sexo',
+        'telefono'
+    ];
+
 }
